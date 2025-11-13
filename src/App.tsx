7902b06c7bbf8588fe77b692b2c691e798342bc2
Maria <mjuliamedeiros.2016@gmail.com>
2025-11-13 09:30:52 -0300

    :blossom: Adição Home, NavBar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
 
-
 function App() {
+  
 
-
-  return ( 
+  return (
     <>
-    <Home />    
+      <Navbar />
+      <Home />
+      <Footer />
     </>
-
   )
 }
 
