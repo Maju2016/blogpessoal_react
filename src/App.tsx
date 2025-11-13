da9e319b03249e9973183c2ca273fcfe8106d1b1
Maria <mjuliamedeiros.2016@gmail.com>
2025-11-13 12:36:44 -0300

    :tulip: Adicionando Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div className="min-h-[80vh]">
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+      </div>
       <Footer />
+    </BrowserRouter>
     </>
   )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+import { GithubLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
 
 
 function Footer() {
@@ -14,9 +14,12 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/mariajulia-medeiros/" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://github.com/Maju2016" target="_blank">
+                            <GithubLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
